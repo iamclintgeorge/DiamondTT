bfaf604da57452c8c06d2b195e6945fee1c2a339
Clint George <clintbgeorge@gmail.com>
2024-07-19 20:28:13 +0530

    Added Tailwind CSS
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div>
+      {/* Here goes the logo */}
+    </div>
+      <ul className='list-none'>
+        <li>Home</li>
+        <li>Contact Us</li>
+        <li>Packages</li>
+        <li>Design Itinerary</li>
+      </ul>
+    </>
+  )
+}
+
+export default Header
