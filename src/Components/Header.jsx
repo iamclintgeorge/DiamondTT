498f3dc974502140b43ccc6d003fcdd097366c2c
Clint George <clintbgeorge@gmail.com>
2024-07-20 21:47:33 +0530

    Created Header
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,15 +3,19 @@ import React from 'react'
 const Header = () => {
   return (
     <>
+    <div className='flex justify-between mt-5'>
     <div>
-      {/* Here goes the logo */}
+      <img className='flex-1 ml-10 w-10' src="/src/assets/Fleur-de-lis-fill-white.png" alt="Logo" />
     </div>
-      <ul className='list-none'>
+    <div className='w-full ml-52 content-center'>
+    <ul className='list-none flex justify-around text-white'>
         <li>Home</li>
         <li>Contact Us</li>
         <li>Packages</li>
         <li>Design Itinerary</li>
       </ul>
+    </div>
+    </div>
     </>
   )
 }
