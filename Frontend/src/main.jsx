322cd87f1e106909d8b5e6be018317935fa252e2
Clint George <clintbgeorge@gmail.com>
2024-12-25 17:46:15 +0530

    Minor Changes
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Pages/App.jsx'
-import {createBrowserRouter, Route, Routes, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
 import Signup from './Pages/Signup.jsx'
 import Login from './Pages/Login.jsx'
